8ed8466cb82b95a914ef8bafff51020ceccf4a25
Guilherme Senna Cruz <53920452+GuilhermeSenna@users.noreply.github.com>
2021-06-07 23:51:12 -0300

    Final additions
    - on_progress function to show the download progress (does not show in real time as the page takes a while to load)
    
    - Fixed bug of getting video id if there is no '&' in the string
    
    - MM:SS and HH:MM:SS formatting depending on video length
    
    - Percentage of approval based on rating
    
    - Video Size in MB and KB

--- YoutubeDownloader.py
+++ YoutubeDownloader.py
@@ -2,60 +2,95 @@ import streamlit as st
 from pytube import YouTube
 import locale
 import SessionState
+import time
 
-session_state = SessionState.get(url='', yt=None)
+
+session_state = SessionState.get(url='', yt=None, progress=0)
 
 
-def show_progress_bar(stream, chunk, file_handler, bytes_remaining):
-    return  # do work
+previousprogress = 0
+def on_progress(stream, chunk, bytes_remaining):
+    global previousprogress
+    total_size = stream.filesize
+    bytes_downloaded = total_size - bytes_remaining
+
+    liveprogress = (int)(bytes_downloaded / total_size * 100)
+    if liveprogress > previousprogress:
+        previousprogress = liveprogress
+        session_state.progress = liveprogress
+
 
 def main():
     st.title("Youtube Thumbnail Downloader")
 
-    st.text('Enter a Youtube Video Complete URL')
+    # st.text('')
 
-    url = 'https://www.youtube.com/watch?v=LXb3EKWsInQ&ab_channel=Jacob%2BKatieSchwarzJacob%2BKatieSchwarz'
+    url = st.text_input('Enter a Youtube Video Complete URL')
 
-    if not session_state.url or session_state == url:
-        session_state.url = url
+    try:
 
-        # if st.button('Buscar o link'):
-        session_state.yt = YouTube(url)
+        if not session_state.url or session_state.url != url:
+            print('to aqui agora')
+            session_state.url = url
 
-    st.header('Informações sobre o vídeo')
+            # if st.button('Buscar o link'):
+            session_state.yt = YouTube(url)
 
-    st.markdown('---')
+        st.header('Informações sobre o vídeo')
 
-    id = session_state.url[session_state.url.find('=') + 1:session_state.url.find('&')]
+        st.markdown('---')
 
-    st.image('https://img.youtube.com/vi/'+id+'/maxresdefault.jpg')
+        if '&' in session_state.url:
+            id = session_state.url[session_state.url.find('=') + 1:session_state.url.find('&')]
+        else:
+            id = session_state.url[session_state.url.find('=') + 1:]
 
-    # Title of video
-    st.markdown(f'**Title:** {session_state.yt.title}')
+        st.image('https://img.youtube.com/vi/'+id+'/maxresdefault.jpg')
 
-    locale.setlocale(locale.LC_ALL, "en_US.UTF-8")
-    # Number of views of video
-    st.markdown(f'**Number of views:** {session_state.yt.views:n}')
+        # Title of video
+        st.markdown(f'**Title:** {session_state.yt.title}')
 
-    # Length of the video
-    st.markdown(f'**Length of video:** {session_state.yt.length} seconds')
+        locale.setlocale(locale.LC_ALL, "en_US.UTF-8")
+        # Number of views of video
+        st.markdown(f'**Number of views:** {session_state.yt.views:n}')
 
-    # Rating
-    st.markdown(f'**Ratings:** {session_state.yt.rating:.2f} of 5')
+        # Length of the video
+        if session_state.yt.length < 3600:
+            st.markdown(f'**Length of video:** {time.strftime("%M:%S", time.gmtime(session_state.yt.length))} Min')
+        else:
+            st.markdown(f'**Length of video:** {time.strftime("%H:%M:%S", time.gmtime(session_state.yt.length))} Hour')
 
-    # Description of video
-    with st.beta_expander('Descrição do vídeo'):
-        st.text(f'Description: \n\n{session_state.yt.description}')
+        # Rating
+        st.markdown(f'**Ratings:** {session_state.yt.rating:.2f} of 5 ({int((session_state.yt.rating/5)*100)}% approval)')
 
-    videos = session_state.yt.streams.filter(progressive=True).order_by('resolution').desc()
+        # Description of video
+        with st.beta_expander('Descrição do vídeo'):
+            st.text(f'Description: \n\n{session_state.yt.description}')
 
-    # st.write(videos[0].res)
+        videos = session_state.yt.streams.filter(progressive=True).order_by('resolution').desc()
 
-    radio = st.radio('Selecione a qualidade desejada: ', videos)
+        # st.write(videos[0].res)
 
-    if st.button('Download'):
-        st.write(radio.download())
-        st.progress(radio.register_on_progress_callback(show_progress_bar))
+        st.header('Opções de download')
+
+        st.markdown('---')
+
+        radio = st.radio('Selecione a qualidade desejada: ', videos)
+
+        if round(radio.filesize/(1024*1024)) != 0:
+            st.write(f'Tamanho do vídeo: {round(radio.filesize/(1024*1024))} MB')
+        else:
+            st.write(f'Tamanho do vídeo: {round(radio.filesize / (1024))} KB')
+
+        if st.button('Download'):
+            session_state.yt.register_on_progress_callback(on_progress)
+            st.write(radio.download())
+
+            st.progress(session_state.progress)
+
+    except:
+        pass
+
 
 # yt.register_on_progress_callback(show_progress_bar)
 
